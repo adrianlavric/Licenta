3cd35a61faa90ceb1712f5bb524b87b7742c1bb7
Adrian Lavric <adrian.lavric33@gmail.com>
2025-05-22 01:31:51 +0300

    improve prediction visualization

--- app.py
+++ app.py
@@ -16,28 +16,28 @@ model = tf.keras.models.load_model(MODEL_PATH)
 IMG_SIZE = (224, 224)
 
 flower_classes = [
-        "pink primrose", "hard-leaved pocket orchid", "canterbury bells", "sweet pea",
-        "english marigold", "tiger lily", "moon orchid", "bird of paradise", "monkshood",
-        "globe thistle", "snapdragon", "colt's foot", "king protea", "spear thistle",
-        "yellow iris", "globe-flower", "purple coneflower", "peruvian lily", "balloon flower",
-        "giant white arum lily", "fire lily", "pincushion flower", "fritillary",
-        "red ginger", "grape hyacinth", "corn poppy", "prince of wales feathers",
-        "stemless gentian", "artichoke", "sweet william", "carnation", "garden phlox",
-        "love in the mist", "mexican aster", "alpine sea holly", "ruby-lipped cattleya",
-        "cape flower", "great masterwort", "siam tulip", "lenten rose", "barbeton daisy",
-        "daffodil", "sword lily", "poinsettia", "bolero deep blue", "wallflower",
-        "marigold", "buttercup", "oxeye daisy", "common dandelion", "petunia",
-        "wild pansy", "primula", "sunflower", "pelargonium", "bishop of llandaff",
-        "gaura", "geranium", "orange dahlia", "pink-yellow dahlia", "cautleya spicata",
-        "japanese anemone", "black-eyed susan", "silverbush", "californian poppy",
-        "osteospermum", "spring crocus", "bearded iris", "windflower", "tree poppy",
-        "gazania", "azalea", "water lily", "rose", "thorn apple", "morning glory",
-        "passion flower", "lotus", "toad lily", "anthurium", "frangipani", "clematis",
-        "hibiscus", "columbine", "desert-rose", "tree mallow", "magnolia", "cyclamen",
-        "watercress", "canna lily", "hippeastrum", "bee balm", "ball moss",
-        "foxglove", "bougainvillea", "camellia", "mallow", "mexican petunia",
-        "bromelia", "blanket flower", "trumpet creeper", "blackberry lily"
-    ]
+    "pink primrose", "hard-leaved pocket orchid", "canterbury bells", "sweet pea",
+    "english marigold", "tiger lily", "moon orchid", "bird of paradise", "monkshood",
+    "globe thistle", "snapdragon", "colt's foot", "king protea", "spear thistle",
+    "yellow iris", "globe-flower", "purple coneflower", "peruvian lily", "balloon flower",
+    "giant white arum lily", "fire lily", "pincushion flower", "fritillary",
+    "red ginger", "grape hyacinth", "corn poppy", "prince of wales feathers",
+    "stemless gentian", "artichoke", "sweet william", "carnation", "garden phlox",
+    "love in the mist", "mexican aster", "alpine sea holly", "ruby-lipped cattleya",
+    "cape flower", "great masterwort", "siam tulip", "lenten rose", "barbeton daisy",
+    "daffodil", "sword lily", "poinsettia", "bolero deep blue", "wallflower",
+    "marigold", "buttercup", "oxeye daisy", "common dandelion", "petunia",
+    "wild pansy", "primula", "sunflower", "pelargonium", "bishop of llandaff",
+    "gaura", "geranium", "orange dahlia", "pink-yellow dahlia", "cautleya spicata",
+    "japanese anemone", "black-eyed susan", "silverbush", "californian poppy",
+    "osteospermum", "spring crocus", "bearded iris", "windflower", "tree poppy",
+    "gazania", "azalea", "water lily", "rose", "thorn apple", "morning glory",
+    "passion flower", "lotus", "toad lily", "anthurium", "frangipani", "clematis",
+    "hibiscus", "columbine", "desert-rose", "tree mallow", "magnolia", "cyclamen",
+    "watercress", "canna lily", "hippeastrum", "bee balm", "ball moss",
+    "foxglove", "bougainvillea", "camellia", "mallow", "mexican petunia",
+    "bromelia", "blanket flower", "trumpet creeper", "blackberry lily"
+]
 
 
 def preprocess_image(img_data):
@@ -81,7 +81,8 @@ def predict():
             'predictions': [{
                 'class': pred[0],
                 'confidence': round(pred[1], 2)
-            } for pred in top_predictions]
+            } for pred in top_predictions],
+            'flower_classes': flower_classes
         }
 
         return jsonify(result)
